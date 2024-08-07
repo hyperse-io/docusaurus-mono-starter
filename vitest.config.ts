5c54840769caacf22cb99fa18658bfb07abbcb36
tianyingchun <tianyingchun@outlook.com>
2024-08-07 12:00:21 +0800

    chore: add test coveragge

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,9 +9,12 @@ export default defineConfig({
       '@/': new URL('./src/', import.meta.url).pathname,
     },
     coverage: {
-      provider: 'istanbul', // or 'v8'
-      reporter: ['text', 'json', 'html'],
       include: ['src/**'],
+      provider: 'istanbul', // or 'v8'
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
     },
     include: ['**/?(*.){test,spec}.?(c|m)[jt]s?(x)'],
   },
